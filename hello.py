435dec86caf2664772c980efe8108b9b59903096
Vinson Qi <qszboss@foxmail.com>
2020-05-06 16:09:58 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+# 类属性中设置property() 使用这个可以直接 . 出来对应的属性信息
+# global X
+# X = 3
+# import scipy
+# import pandas
+# import sklearn
+# import keras.backend as k
+# import tensorflow
+
+
+import tensorflow as tf
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
+
+
+
